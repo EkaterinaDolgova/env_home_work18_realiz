38b18799f3fb379b9a46c337cb9af4736af62d51
katya <kotenokches@rambler.ru>
2022-05-12 13:19:16 +0500

    ДЗ 18 выполнена

---
+++ src/skyro/company/Sort.java
@@ -0,0 +1,46 @@
+package skyro.company;
+
+public class Sort {
+    public static int[] mergeSort(int[] arr) {
+        if (arr.length < 2) {
+            return null;
+        }
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+
+        for (int i = 0; i < left.length; i++) {
+            left[i] = arr[i];
+        }
+
+        for (int i = 0; i < right.length; i++) {
+            right[i] = arr[mid + i];
+        }
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(arr, left, right);
+        return arr;
+    }
+
+    public static int[] merge(int[] arr, int[] left, int[] right) {
+        int mainP = 0;
+        int leftP = 0;
+        int rightP = 0;
+        while (leftP < left.length && rightP < right.length) {
+            if (left[leftP] <= right[rightP]) {
+                arr[mainP++] = left[leftP++];
+            } else {
+                arr[mainP++] = right[rightP++];
+            }
+        }
+        while (leftP < left.length) {
+            arr[mainP++] = left[leftP++];
+        }
+        while (rightP < right.length) {
+            arr[mainP++] = right[rightP++];
+        }
+        return arr;
+    }
+}
